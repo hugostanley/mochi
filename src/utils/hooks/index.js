d22cf055be6d1089169634de7a21f9b8669e70ea
hugostanley <stanleyhugo06@gmail.com>
2023-10-24 00:02:53 +0800

    feat: create custom hook for accessing and modifying localstorage

---
+++ src/utils/hooks/index.js
@@ -0,0 +1,1 @@
+export { useLocalStorage, getLocalStorage, updateLocalStorage } from './useLocalStorage'
---
+++ src/utils/hooks/useLocalStorage.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+export function useLocalStorage(key, defaultValue) {
+    const [data, setData] = useState(() => {
+        const value = localStorage.getItem(key)
+
+        if (value) {
+            return JSON.parse(value)
+        } else {
+            localStorage.setItem(key, JSON.stringify(defaultValue))
+
+            return defaultValue
+        }
+    })
+
+    const setLocalStorage = (newValueOrFunc) => {
+        let updatedValue
+
+        if (typeof updatedValue === 'function') {
+            updatedValue = newValueOrFunc(data)
+        } else {
+            updatedValue = newValueOrFunc
+        }
+
+        localStorage.setItem(key, JSON.stringify(updatedValue))
+        setData(updatedValue)
+    }
+
+    return [data, setLocalStorage]
+}
+
+export function updateLocalStorage(key, newValue) {
+    localStorage.setItem(key, JSON.stringify(newValue))
+}
+
+export function getLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key) || '[]')
+}
