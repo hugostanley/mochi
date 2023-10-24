f923778977c06463362ab9f713458305638e5730
hugostanley <stanleyhugo06@gmail.com>
2023-10-24 19:55:08 +0800

    chore: make use of global paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,30 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-import App from '@/App'
-import { Login, SignUp } from '@/pages'
-import { signUpAction } from '@/utils/actions/signUpAction'
-
+import { LandingPage, Login, SignUp } from '@/pages'
+import { globals, signUpAction } from '@/utils'
 
 const router = createBrowserRouter([
     {
-        path: 'login',
-        element: <Login></Login>
+        path: globals.paths.home,
+        element: <LandingPage />
     },
     {
-        path: '/app',
-        element: <App />,
-        action: async ({ request }) => {
-            let formdata = await request.formData()
-            console.log(...formdata)
-            console.log(formdata.get('username'))
-
-            return <App />
-        }
+        path: globals.paths.login,
+        element: <Login />
     },
     {
-        path: '/signup',
+        path: globals.paths.signup,
         action: signUpAction,
         element: <SignUp />
     }
