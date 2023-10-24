e4986671350ec6ff2867e65032d47c34a7c05984
hugostanley <stanleyhugo06@gmail.com>
2023-10-24 19:54:13 +0800

    feat: copy login form from signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,28 @@
+import { FormProvider, Button, Spacer } from '@/components'
+import { globals } from '@/utils'
 import React from 'react'
-import { Form } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 export function Login() {
+    const navigate = useNavigate()
     return (
-        <Form method="post" action="/app">
-            <input name='username' placeholder='username'/>
-            <input name='email' placeholder='email'/>
-            <button type="submit">Submit</button>
-        </Form>
+        <div className='w-full h-screen flex items-center justify-center'>
+            <FormProvider>
+                <FormProvider.Header>Log in</FormProvider.Header>
+                <FormProvider.Form method="post" action="/login">
+                    <FormProvider.Field>
+                        <FormProvider.Label>Email</FormProvider.Label>
+                        <FormProvider.Input type="text" name="email" />
+                    </FormProvider.Field>
+                    <FormProvider.Field>
+                        <FormProvider.Label>Password</FormProvider.Label>
+                        <FormProvider.Input type="password" name="password" />
+                    </FormProvider.Field>
+                    <Button variant="primary">Log in</Button>
+                </FormProvider.Form>
+                <Spacer text="or" />
+                <Button variant="secondary" onClick={() => navigate(globals.paths.signup)}>Create an account</Button>
+            </FormProvider>
+        </div>
     )
 }
