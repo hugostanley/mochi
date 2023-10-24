26ec1a080586a69a4cc172bd48add9cb9cbe25ac
hugostanley <stanleyhugo06@gmail.com>
2023-10-24 10:37:17 +0800

    feat: make use of form component in signup

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,3 +1,4 @@
+import { FormProvider } from '@/components'
 import React from 'react'
 import { useActionData } from 'react-router-dom'
 import { Form } from 'react-router-dom'
@@ -5,12 +6,24 @@ import { Form } from 'react-router-dom'
 export function SignUp() {
     const errors = useActionData()
     return (
-        <Form action="/signup" method='post'>
-            <input placeholder='username' name="username" />
-            <input placeholder='email' name="email" />
-            {errors?.email && <span>{errors.email}</span>}
-            <input placeholder="password" name="password" />
-            <button type="submit">Submit</button>
-        </Form>
+        <div className='w-full h-screen flex items-center justify-center'>
+            <FormProvider method="post" action="/signup">
+                <FormProvider.Header>Sign Up</FormProvider.Header>
+                <FormProvider.Form>
+                    <FormProvider.Field>
+                        <FormProvider.Label>Username</FormProvider.Label>
+                        <FormProvider.Input type="text" name="username" />
+                    </FormProvider.Field>
+                    <FormProvider.Field>
+                        <FormProvider.Label>Email</FormProvider.Label>
+                        <FormProvider.Input type="text" name="email" />
+                    </FormProvider.Field>
+                    <FormProvider.Field>
+                        <FormProvider.Label>Password</FormProvider.Label>
+                        <FormProvider.Input type="password" name="password" />
+                    </FormProvider.Field>
+                </FormProvider.Form>
+            </FormProvider>
+        </div>
     )
 }
