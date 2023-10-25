bbeadb00fd0518079139e9dd1206625321b74264
hugostanley <stanleyhugo06@gmail.com>
2023-10-25 20:40:39 +0800

    feat: add user login management

--- src/utils/actions/loginActions.js
+++ src/utils/actions/loginActions.js
@@ -1,8 +1,37 @@
-export async function loginAction({request}){
+import { getLocalStorage, globals, updateLocalStorage } from "@/utils"
+import { redirect } from "react-router-dom"
+import { toast } from "react-toastify"
+
+export async function loginAction({ request }) {
     let formData = await request.formData()
-    let adminList = await localStorage.getItem('adminList')
+    const adminList = getLocalStorage('adminList')
 
-    if(adminList){
+    if (!adminList) {
+        toast.error("Please create an admin account", {
+            position: "top-center"
+        })
+        return redirect(globals.paths.signup)
+    }
 
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    if (password && email) {
+        const foundUser = adminList.find((item) => item.password === password && item.email === email)
+
+        if (foundUser) {
+            updateLocalStorage("userSignedIn", true)
+            updateLocalStorage("currentUser", foundUser)
+            toast.success("Signed in succesfully!", { position: "top-center" })
+            return redirect(globals.paths.dashboard)
+        } else {
+            toast.error("User not found", { position: "top-center" })
+            return redirect(globals.paths.login)
+        }
+    } else {
+        toast.error("Please type your email and password", {
+            position: "top-center"
+        })
+        return redirect(globals.paths.login)
     }
 }
