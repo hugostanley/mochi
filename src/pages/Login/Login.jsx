b555ed04a16bde56ae33aea967f3374163884af3
hugostanley <stanleyhugo06@gmail.com>
2023-10-25 20:40:18 +0800

    chore: add required errors property and error message component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,21 @@
 import { FormProvider, Button, Spacer } from '@/components'
 import { globals } from '@/utils'
 import React from 'react'
+import { useActionData } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
 export function Login() {
+    const errors = useActionData()
     const navigate = useNavigate()
     return (
         <div className='w-full h-screen flex items-center justify-center'>
-            <FormProvider>
+            <FormProvider errors={errors}>
                 <FormProvider.Header>Log in</FormProvider.Header>
                 <FormProvider.Form method="post" action="/login">
                     <FormProvider.Field>
                         <FormProvider.Label>Email</FormProvider.Label>
                         <FormProvider.Input type="text" name="email" />
+                        <FormProvider.ErrorMessage forInput="email"/>
                     </FormProvider.Field>
                     <FormProvider.Field>
                         <FormProvider.Label>Password</FormProvider.Label>
