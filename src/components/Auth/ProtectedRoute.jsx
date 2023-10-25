3cdeae9859c9737b01209b2ca88b7be3390b5cb6
hugostanley <stanleyhugo06@gmail.com>
2023-10-25 20:27:31 +0800

    feat: create protectedroute component

---
+++ src/components/Auth/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { getLocalStorage, globals } from '@/utils'
+import { Navigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+
+export function ProtectedRoute({ children }) {
+    const userSignedIn = getLocalStorage("userSignedIn")
+    const currentUser = getLocalStorage("currentUser")
+
+    if (!userSignedIn && !currentUser) {
+        toast.error('Please login to access the page', { position: "top-center" })
+        return <Navigate to={globals.paths.login} replace={true} />
+    }
+
+    return (
+        { children }
+    )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './Form'
 export * from './Button'
 export * from './Spacer'
+export * from './Auth'
