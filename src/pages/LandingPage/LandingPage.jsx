eee95041939f1fde0c619041b3e14c2cd6b51ab0
hugostanley <stanleyhugo06@gmail.com>
2023-10-25 20:11:31 +0800

    style(landinpage): add simple navigating styles to landingpage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom'
 
 export function LandingPage() {
     return (
-        <div>
-            <h1>This is the landing page</h1>
-            <Link to={globals.paths.login}>Login</Link>
-            <Link to={globals.paths.signup}>Sign up</Link>
+        <div className="w-full h-screen flex items-center justify-center">
+            <div className='flex flex-col shadow-sm border border-gray-300 w-64 h-64 rounded-xl items-center justify-center bg-white'>
+                <h1>This is the landing page</h1>
+                <Link to={globals.paths.login}>Login</Link>
+                <Link to={globals.paths.signup}>Sign up</Link>
+            </div>
         </div>
     )
 }
